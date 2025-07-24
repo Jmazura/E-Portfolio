e04b50cb4a5d6b5f431647494342a4b4c63cd598
Yugiboyy <azurajhonmichael33@gmail.com>
2025-07-24 11:25:05 +0800

    About Page Created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+
+import Image from 'next/image'
+
+export default function About() {
+	
+	return (
+
+		<main className="h-screen flex text-center items-center justify-center gap-4">
+			
+			<div className="rounded border-r-2 bg-azura-accent">
+				<h1>
+					Jhon
+				</h1>
+			</div>
+
+
+			<div className="w-2/5">
+				<p>
+				Amet non consequat adipisicing laboris dolor aliqua nulla cupidatat irure veniam minim labore tempor cillum irure do sed tempor.
+				</p>
+
+			</div>
+		
+		</main>
+		)
+}
