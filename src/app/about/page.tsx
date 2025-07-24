919e9104dcf94bd2ff9c35ce2b22d76bb7ba3d8c
Yugiboyy <azurajhonmichael33@gmail.com>
2025-07-24 11:32:55 +0800

    About Page template with image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,16 +7,21 @@ export default function About() {
 
 		<main className="h-screen flex text-center items-center justify-center gap-4">
 			
-			<div className="rounded border-r-2 bg-azura-accent">
-				<h1>
-					Jhon
-				</h1>
+			<div className="w-48 h-48 rounded-full border-4 border-azura-border bg-azura-surface overflow-hidden">
+				<Image
+		          src="/images/me.jfif"
+		          alt="Yugiboyy"
+		          width={256}
+		          height={256}
+		          className="object-cover w-full h-full"
+		        />
 			</div>
 
 
 			<div className="w-2/5">
 				<p>
 				Amet non consequat adipisicing laboris dolor aliqua nulla cupidatat irure veniam minim labore tempor cillum irure do sed tempor.
+				Lorem ipsum aute nisi eiusmod ex ea quis et esse dolore irure est voluptate pariatur elit irure laborum veniam deserunt sunt occaecat mollit laborum proident occaecat nostrud anim reprehenderit aute.
 				</p>
 
 			</div>
